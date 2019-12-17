d5019e271c81620be4fd368369a7c3b925637218
Anatoly512 <56732684+Anatoly512@users.noreply.github.com>
2019-12-17 03:28:54 +0200

    Training experiments

---
+++ src/NewClass.java
@@ -0,0 +1,20 @@
+public class NewClass {
+
+    public int NewClass(int a) {
+
+        int b;
+        b = a;
+
+        System.out.println("\n CONSTRUCTOR  \n");
+        return b;
+
+    }
+
+    public void printMyString () {
+
+        System.out.println("\n My String  \n");
+
+    }
+
+
+}
---
+++ src/Training.java
@@ -0,0 +1,32 @@
+import com.sun.tools.javac.Main;
+
+import javax.swing.*;
+
+public class Training {
+
+    private static int a;
+
+    NewClass newClass = new NewClass();
+
+    public static void main(String[] args) {
+
+        NewClass myConstructor = new NewClass();
+        Training myO = new Training();
+
+      //  new NewClass();
+      //  NewClass.NewClass(b);
+      //  NewClass.printMyString();
+
+        myO.doMy();
+
+         }
+
+         private void doMy () {
+
+         newClass.NewClass(a);
+         newClass.printMyString();
+
+
+         }
+
+}
